aa971bb724b5eeffc39601456b5ba2c9a2715247
Kartha <ykartha6@cos-4a10657.cos.gatech.edu>
2023-11-03 14:31:42 -0400

    test

---
+++ scripts/Conditional NF on leak no leak.jl
@@ -0,0 +1,346 @@
+using DrWatson
+# @quickactivate "wavelet_gen"
+# import Pkg; Pkg.instantiate()
+
+# include(projectdir("src/utils.jl"))
+
+#import wavelet_gen
+using Flux
+using LinearAlgebra
+using Random
+using JLD2
+using Statistics
+using ImageQualityIndexes 
+using PyPlot
+using SlimPlotting
+using InvertibleNetworks
+# using UNet
+using BSON 
+# using Wavelets
+using StatsBase
+using Distributions
+using Images
+matplotlib.use("Agg");
+plot_path = plotsdir("/nethome/ykartha6/SLURM_CRUYFF/Plotscond/training_64x64_cond_iterativeooooo");
+
+Random.seed!(1234)
+# Y_train, X_train, Y_val, X_val, Y_test, X_test = get_data_train_64_gen()
+
+function posterior_sampler(G, y, size_x; device=gpu, num_samples=1, batch_size=16)
+    # make samples from posterior for train sample
+	X_forward = randn(Float32, size_x[1:end-1]...,batch_size) |> device
+    Y_train_latent_repeat = repeat(y |>cpu, 1, 1, 1, batch_size) |> device
+    _, Zy_fixed_train, _ = G.forward(X_forward, Y_train_latent_repeat); #needs to set the proper sizes here
+    X_post_train = zeros(Float32, size_x[1:end-1]...,num_samples)
+    for i in 1:div(num_samples, batch_size)
+    	ZX_noise_i = randn(Float32, size_x[1:end-1]...,batch_size)|> device
+   		X_post_train[:,:,:, (i-1)*batch_size+1 : i*batch_size] = G.inverse(
+        	ZX_noise_i,
+        	Zy_fixed_train
+    		)[1] |> cpu;
+	end
+	X_post_train
+end
+
+
+
+function normalize_images(data)
+    num_images, height, width, channels = size(data)
+    
+    for i in 1:num_images
+        min_vals = minimum(data[i, :, :, :], dims=(1, 2, 3))
+        max_vals = maximum(data[i, :, :, :], dims=(1, 2, 3))
+        data[i, :, :, :] .= (data[i, :, :, :] .- min_vals) ./ (max_vals .- min_vals)
+    end
+    
+    return data
+end
+  
+  
+  #Loading Data
+data_path= "/nethome/ykartha6/SLURM_CRUYFF/CompassShot.jld2"
+  
+train_X = jldopen(data_path, "r")["X"]
+train_y = jldopen(data_path, "r")["Y"]
+  
+test_x1 = zeros(Float32, 2048, 512, 1,301)
+  
+for i=1:301
+    sigma = 1.0
+    test_x1[:,:,:,i] = imresize(imfilter(train_X[:,:,i+1499],KernelFactors.gaussian((sigma,sigma))),(2048,512))
+end
+test_y = train_y[1500:end,:]
+  
+  
+train_x1 = zeros(Float32, 2048, 512, 1,1504)
+  
+for i=1:1504
+    sigma = 1.0
+    train_x1[:,:,:,i] = imresize(imfilter(train_X[:,:,i],KernelFactors.gaussian((sigma,sigma))),(2048,512))
+end
+  
+train_y = train_y[1:1504,:]
+train_Y = ones(Float32,2048,512,1,1504)
+for i=1:1504
+    if train_y[i,1]==1.0
+        train_Y[:,:,:,i]= train_Y[:,:,:,i] .*2
+    end
+end
+
+test_Y = ones(Float32,2048,512,1,300)
+for i=1:300
+    if test_y[i,1]==1.0
+        test_Y[:,:,:,i]=test_Y[:,:,:,i] .* 2
+    end
+end
+
+
+train_x1 = normalize_images(train_x1)
+test_x1 = normalize_images(test_x1)
+# Choose params
+batch_size = 2
+nx,ny = 2048, 512
+N = nx*ny;
+
+n_train = 1504
+
+n_batches = cld(n_train,batch_size)
+
+# Training hyperparameters 
+n_epochs     = 100
+device = gpu
+lr = 1f-4
+lr_step   = 10
+lr_rate = 0.75f0
+clipnorm_val = 10f0
+noise_lev_x  = 0.005f0
+noise_lev_y  = 0.00f0
+split_scales = true
+
+#User params
+save_every   = 5
+plot_every   = 1
+
+# Number of samples to test conditional mean quality metric on.
+n_condmean = 32
+posterior_samples = 32 
+
+# Create conditional network
+K = 9
+L = 5# l=5 is better
+n_hidden = 64
+low = 0.5f0
+n_in = 1
+n_out = 1
+#Choose model
+G = NetworkConditionalGlow(n_out, n_in, n_hidden,  L, K; split_scales=split_scales, activation=SigmoidLayer(low=low,high=1.0f0));
+G = G |> device;
+
+# Optimizer
+opt_adam = "adam"
+opt = Flux.Optimiser(ExpDecay(lr, lr_rate, n_batches*lr_step, 1f-6), ClipNorm(clipnorm_val), ADAM(lr))
+
+# Training logs 
+loss   = [];
+logdet_train = [];
+ssim   = [];
+l2_cm  = [];
+
+loss_val   = [];
+logdet_val = [];
+ssim_val   = [];
+l2_cm_val  = [];
+
+vmax_val = 200
+vmin_val = -125
+num_post_samples=32
+# X_val_latent = X_val[:,:,:,1:8]
+# Y_val_latent = Y_val[:,:,:,1:8]
+
+# X_train_latent = X_train[:,:,:,1:8]
+# Y_train_latent = Y_train[:,:,:,1:8]
+
+lat_vmax = 8
+lat_vmin = -8
+
+
+#Training
+function sigmoid_schedule(t , T , tau =0.6 , start =0 , end_t =3 , clip_min =1f-9) 
+    # A scheduling function based on sigmoid function with a temperature tau .
+        v_start = sigmoid( start / tau )
+        v_end = sigmoid( end_t / tau )
+        return ( v_end - sigmoid(( t /T * ( end_t - start ) + start ) / tau )) / ( v_end - v_start )
+    end
+    tau = 0.6
+
+for e=1:n_epochs# epoch loop
+    # sigma_noise = sigmoid_schedule(e,n_epochs,tau)
+    # alpha_noise = sqrt(1-sigma_noise^2)
+    idx_e = reshape(randperm(n_train), batch_size, n_batches)
+    for b = 1:n_batches # batch loop
+
+    	@time begin
+	        X = train_x1[:, :, :, idx_e[:,b]];
+	        Y = train_Y[:, :, :, idx_e[:,b]];
+	        X .+= noise_lev_x*randn(Float32, size(X));
+            # X = alpha_noise .* X + sigma_noise*randn(Float32, size(X));
+			# Y .+= noise_lev_y*randn(Float32, size(Y));
+      
+	        # Forward pass of normalizing flow
+	        Zx, Zy, lgdet = G.forward(X|> device, Y|> device)
+
+	        # Loss function is l2 norm 
+	        append!(loss, norm(Zx)^2 / (N*batch_size))  # normalize by image size and batch size
+	        append!(logdet_train, -lgdet / N) # logdet is internally normalized by batch size
+
+	        # Set gradients of flow and summary network
+	        #G.backward(Zx / batch_size, Zx, Zy; C_save = Y |> device)
+            G.backward(Zx / batch_size, Zx, Zy;)
+
+	        for p in get_params(G) 
+	          Flux.update!(opt,p.data,p.grad)
+	        end
+	        clear_grad!(G)
+
+	        println("Iter: epoch=", e, "/", n_epochs, ", batch=", b, "/", n_batches, 
+	            "; f l2 = ",  loss[end], 
+                "; lgdet = ", logdet_train[end], "; f = ", loss[end] + logdet_train[end], "\n")
+            
+
+	        Base.flush(Base.stdout)
+    	end
+    end
+
+    # get objective mean metrics over testing batch  
+    # @time l2_val, lgdet_val  = get_loss(G, X_val, Y_val; device=device, batch_size=batch_size)
+    # append!(logdet_val, -lgdet_val)
+    # append!(loss_val, l2_val)
+
+    # # get conditional mean metrics over training batch (takes a bit since you have to do posterior sample but worth it in order to catch overfitting)
+    # @time cm_l2_train, cm_ssim_train = get_cm_l2_ssim(G, X_train[:,:,:,1:n_condmean], Y_train[:,:,:,1:n_condmean]; device=device, num_samples=posterior_samples )
+    # append!(ssim, cm_ssim_train)
+    # append!(l2_cm, cm_l2_train)
+
+    # # get conditional mean metrics over testing batch (takes a bit since you have to do posterior sample but worth it in order to catch overfitting)
+    # @time cm_l2_val, cm_ssim_val  = get_cm_l2_ssim(G, X_val[:,:,:,1:n_condmean], Y_val[:,:,:,1:n_condmean]; device=device, num_samples=posterior_samples )
+    # append!(ssim_val, cm_ssim_val)
+    # append!(l2_cm_val, cm_l2_val)
+
+    if(mod(e,plot_every)==0) 
+        for (test_x, test_y, file_str) in [[train_x1,train_Y, "train"], [test_x1, test_Y, "test"]]
+            num_cols = 8
+            plots_len = 2
+            all_sampls = size(test_x)[end]
+            fig = figure(figsize=(25, 5)); 
+            for (i,ind) in enumerate((1:div(all_sampls,3):all_sampls)[1:plots_len])
+                x = test_x[:,:,:,ind:ind] 
+                y = test_y[:,:,:,ind:ind]
+                y .+= noise_lev_y*randn(Float32, size(y));
+        
+                # make samples from posterior for train sample 
+                X_post = posterior_sampler(G,  y, size(x); device=device, num_samples=num_post_samples,batch_size)|> cpu
+                X_post_mean = mean(X_post,dims=4)
+                X_post_std  = std(X_post, dims=4)
+        
+                x_hat = X_post_mean[:,:,1,1]
+                x_gt =  x[:,:,1,1]
+                error_mean = abs.(x_hat-x_gt)
+        
+                ssim_i = round(assess_ssim(x_hat, x_gt),digits=2)
+                rmse_i = round(sqrt(mean(error_mean.^2)),digits=4)
+        
+                y_plot = y[:,:,1,1]'
+                a = maximum(x_gt)
+                b = minimum(x_gt)
+        
+                # subplot(plots_len,num_cols,(i-1)*num_cols+1); imshow(y_plot, vmin=-a,vmax=a,interpolation="none", cmap="gray")
+                # axis("off"); title(L"rtm");colorbar(fraction=0.046, pad=0.04);
+        
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+3); imshow(X_post[:,:,1,1],vmin=b,vmax=a, interpolation="none", cmap="gray")
+                axis("off"); title("Posterior sample"); colorbar(fraction=0.046, pad=0.04);
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+4); imshow(X_post[:,:,1,2], vmin=b,vmax=a,interpolation="none", cmap="gray")
+                axis("off");title("Posterior sample") ; colorbar(fraction=0.046, pad=0.04);title("Posterior sample")
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+5); imshow(x_gt, vmin=b,vmax=a,   interpolation="none", cmap="gray")
+                axis("off"); title(L"Reference $\mathbf{x^{*}}$") ; colorbar(fraction=0.046, pad=0.04);
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+6); imshow(x_hat , vmin=b,vmax=a,   interpolation="none", cmap="gray")
+                axis("off"); title("Posterior mean | SSIM="*string(ssim_i)) ; colorbar(fraction=0.046, pad=0.04);
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+7); imshow(error_mean , vmin=0,vmax=nothing, interpolation="none", cmap="gray")
+                axis("off");title("Error | RMSE="*string(rmse_i)) ; cb = colorbar(fraction=0.046, pad=0.04);
+        
+                subplot(plots_len,num_cols,(i-1)*num_cols+8); imshow(X_post_std[:,:,1,1] , vmin=0,vmax=nothing,interpolation="none", cmap="gray")
+                axis("off"); title("Standard deviation") ;cb =colorbar(fraction=0.046, pad=0.04);
+
+        end
+    
+        tight_layout()
+        fig_name = @strdict e lr n_hidden L K batch_size
+        safesave(joinpath(plot_path, savename(fig_name; digits=6)*"_"*file_str*".png"), fig); close(fig)
+
+        
+        XAD = train_x1[:,:,:,1:2]
+        XA = train_x1[:,:,:,4:5]
+
+        YAD = train_Y[:,:,:,1:2]
+        YA = train_Y[:,:,:,4:5]
+
+        shot_rec = zeros(Float32,2048,512,1,4)
+
+        _, Zy_fixed_train, _ = G.forward(XAD |> device, YAD |> device); #needs to set the proper sizes here
+        ZX_noise_i = randn(Float32, 2048,512,1,2)|> device
+        shot_rec[:,:,:, 1:2] = G.inverse( ZX_noise_i,Zy_fixed_train)[1] |> cpu;
+
+        _, Zy_fixed_train, _ = G.forward(XA |> device, YA |> device); #needs to set the proper sizes here
+        ZX_noise_i = randn(Float32, 2048,512,1,2)|> device
+        shot_rec[:,:,:,3:4] = G.inverse( ZX_noise_i,Zy_fixed_train)[1] |> cpu;
+
+        plot_sdata(shot_rec[:,:,:,1],(0.8,1),vmax=1.0,cbar=true)
+        plt.title("Shot record pred ( vel + den) $e")
+        plt.savefig("/nethome/ykartha6/SLURM_CRUYFF/Plotscond/Shot rec/vel+den$e.png")
+        plt.close()
+
+        plot_sdata(shot_rec[:,:,:,4],(0.8,1),vmax=1.0,cbar=true)
+        plt.title("Shot record pred ( vel) $e")
+        plt.savefig("/nethome/ykartha6/SLURM_CRUYFF/Plotscond/Shot rec/vel$e.png")
+        plt.close()
+
+
+        # sum = loss + logdet
+		# sum_test = loss_test + logdet_test
+
+		# fig1 = figure(figsize=(20,20))
+		# subplot(3,1,1); title("L2 Term: train="*string(loss[end])*" test="*string(loss_test[end]))
+		# plot(loss, label="train");
+		# plot(n_batches:n_batches:n_batches*e, loss_test, label="test"); 
+		# axhline(y=1,color="red",linestyle="--",label="Normal Noise")
+		# xlabel("Parameter Update"); legend();
+		# ylim(0,2)
+
+		# subplot(3,1,2); title("Logdet Term: train="*string(logdet[end])*" test="*string(logdet_test[end]))
+		# plot(logdet);
+		# plot(n_batches:n_batches:n_batches*e, logdet_test);
+		# xlabel("Parameter Update") ;
+
+		# subplot(3,1,3); title("Total Objective: train="*string(sum[end])*" test="*string(sum_test[end]))
+		# plot(sum); 
+		# plot(n_batches:n_batches:n_batches*e, sum_test); 
+		# xlabel("Parameter Update") ;
+
+        # tight_layout()
+		# fig_name = @strdict freeze_conv opt_adam clip_norm  e n_epochs n_train  lr lr_rate lr_step noise_lev n_hidden L K batch_size
+		# safesave(joinpath(plot_path,savename(fig_name; digits=6)*"_log.png"), fig1); close(fig1)
+    end
+
+
+	end
+
+
+end
+
+println("... train_cond_64_gen Done! ...") 
+
+#include("/home/ykartha6/juliacode/Conditional NF on leak no leak.jl")
---
+++ scripts/discrim.jl
@@ -0,0 +1,310 @@
+using DrWatson
+using Random
+using Metalhead
+using Flux
+using Flux.Optimise: Optimiser, ExpDecay, update!, ADAM , gpu
+using LinearAlgebra
+using PyPlot
+using MLDatasets
+using Statistics
+using Images 
+using JLD2
+using BSON: @save 
+using Printf 
+using MLUtils
+using Random; Random.seed!(1)
+
+
+
+model = Chain(
+    Chain([
+      Conv((7, 7), 1 => 64, pad=3, stride=2, bias=false),  # 9_408 parameters
+      BatchNorm(64, relu),            # 128 parameters, plus 128
+      MaxPool((3, 3), pad=1, stride=2),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 64 => 64, pad=1, bias=false),  # 36_864 parameters
+          BatchNorm(64, relu),        # 128 parameters, plus 128
+          Conv((3, 3), 64 => 64, pad=1, bias=false),  # 36_864 parameters
+          BatchNorm(64),              # 128 parameters, plus 128
+        ),
+        identity,
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 64 => 64, pad=1, bias=false),  # 36_864 parameters
+          BatchNorm(64, relu),        # 128 parameters, plus 128
+          Conv((3, 3), 64 => 64, pad=1, bias=false),  # 36_864 parameters
+          BatchNorm(64),              # 128 parameters, plus 128
+        ),
+        identity,
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 64 => 128, pad=1, stride=2, bias=false),  # 73_728 parameters
+          BatchNorm(128, relu),       # 256 parameters, plus 256
+          Conv((3, 3), 128 => 128, pad=1, bias=false),  # 147_456 parameters
+          BatchNorm(128),             # 256 parameters, plus 256
+        ),
+        Chain([
+          Conv((1, 1), 64 => 128, stride=2, bias=false),  # 8_192 parameters
+          BatchNorm(128),             # 256 parameters, plus 256
+        ]),
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 128 => 128, pad=1, bias=false),  # 147_456 parameters
+          BatchNorm(128, relu),       # 256 parameters, plus 256
+          Conv((3, 3), 128 => 128, pad=1, bias=false),  # 147_456 parameters
+          BatchNorm(128),             # 256 parameters, plus 256
+        ),
+        identity,
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 128 => 256, pad=1, stride=2, bias=false),  # 294_912 parameters
+          BatchNorm(256, relu),       # 512 parameters, plus 512
+          Conv((3, 3), 256 => 256, pad=1, bias=false),  # 589_824 parameters
+          BatchNorm(256),             # 512 parameters, plus 512
+        ),
+        Chain([
+          Conv((1, 1), 128 => 256, stride=2, bias=false),  # 32_768 parameters
+          BatchNorm(256),             # 512 parameters, plus 512
+        ]),
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 256 => 256, pad=1, bias=false),  # 589_824 parameters
+          BatchNorm(256, relu),       # 512 parameters, plus 512
+          Conv((3, 3), 256 => 256, pad=1, bias=false),  # 589_824 parameters
+          BatchNorm(256),             # 512 parameters, plus 512
+        ),
+        identity,
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 256 => 512, pad=1, stride=2, bias=false),  # 1_179_648 parameters
+          BatchNorm(512, relu),       # 1_024 parameters, plus 1_024
+          Conv((3, 3), 512 => 512, pad=1, bias=false),  # 2_359_296 parameters
+          BatchNorm(512),             # 1_024 parameters, plus 1_024
+        ),
+        Chain([
+          Conv((1, 1), 256 => 512, stride=2, bias=false),  # 131_072 parameters
+          BatchNorm(512),             # 1_024 parameters, plus 1_024
+        ]),
+      ),
+      Parallel(
+        Metalhead.addrelu,
+        Chain(
+          Conv((3, 3), 512 => 512, pad=1, bias=false),  # 2_359_296 parameters
+          BatchNorm(512, relu),       # 1_024 parameters, plus 1_024
+          Conv((3, 3), 512 => 512, pad=1, bias=false),  # 2_359_296 parameters
+          BatchNorm(512),             # 1_024 parameters, plus 1_024
+        ),
+        identity,
+      ),
+    ]),
+    Chain(
+      AdaptiveMeanPool((1, 1)),
+      MLUtils.flatten,
+      Dense(512 => 2, sigmoid),             # 513_000 parameters
+    ),
+  )
+
+# model = ResNet(18);
+# model = Chain(model.layers[1:end-1],AdaptiveMeanPool((1, 1)),
+# MLUtils.flatten,Dense(512,2,softmax))
+# conv_layer = Conv((7, 7), 1 => 64, pad=3, stride=2, bias=false)
+
+# Define the model
+# model = Chain(
+#     conv_layer
+#     # Add more layers if necessary
+# )
+model = gpu(model)
+
+# Training hyperparameters 
+n_epochs     = 20
+# device = gpu
+lr = 1f-3
+opt = Flux.Optimise.ADAM(lr) |> gpu
+loss(x, y) = Flux.binarycrossentropy(x, y) |> gpu
+
+function normalize_images(data)
+  num_images, height, width, channels = size(data)
+  
+  for i in 1:num_images
+      min_vals = minimum(data[i, :, :, :], dims=(1, 2, 3))
+      max_vals = maximum(data[i, :, :, :], dims=(1, 2, 3))
+      data[i, :, :, :] .= (data[i, :, :, :] .- min_vals) ./ (max_vals .- min_vals)
+  end
+  
+  return data
+end
+
+
+#Loading Data
+data_path= "/home/ykartha6/juliacode/CompassShot.jld2"
+
+train_X = jldopen(data_path, "r")["X"]
+train_y = jldopen(data_path, "r")["Y"]
+
+test_x = zeros(Float32, 2048, 512, 1:1,301)
+
+for i=1:301
+  sigma = 1.0
+  test_x[:,:,:,i] = imresize(imfilter(train_X[:,:,i+1499],KernelFactors.gaussian((sigma,sigma))),(2048,512))
+end
+test_y = train_y[1500:end,:]
+
+
+train_x = zeros(Float32, 2048, 512, 1:1,1504)
+
+for i=1:1504
+  sigma = 1.0
+  train_x[:,:,:,i] = imresize(imfilter(train_X[:,:,i],KernelFactors.gaussian((sigma,sigma))),(2048,512))
+end
+
+train_y = train_y[1:1504,:]
+num_classes = maximum(train_y) + 1
+
+# Perform one-hot encoding using Flux.onehotbatch
+onehot_labels = Flux.onehotbatch(train_y, 0:num_classes-1)
+onehot_labels_test = Flux.onehotbatch(test_y, 0:num_classes-1)
+train_x = normalize_images(train_x)
+test_x = normalize_images(test_x)
+n_train = size(train_x)[end]
+n_test = size(test_x)[end]-1
+batch_size=2
+n_batches = cld(n_train, batch_size) 
+n_batches_test = cld(n_test, batch_size) 
+# Training Loop
+losslist=[]
+accuracylist=[]
+
+losslistpara=[]
+accuracylistpara=[]
+
+losslist_test=[]
+accuracylist_test=[]
+for e=1:n_epochs
+  epoch_loss=0.0
+  epoch_loss_test=0.0
+  correct_predictions=0.0
+  correct_predictions_test=0.0
+  idx_e = reshape(randperm(n_train), batch_size, n_batches)
+    for b = 1:n_batches # batch loop
+        x = train_x[:,:,:,idx_e[:,b]]
+        x .+= 0.001*randn(Float32, size(x))
+        x = x |> gpu
+        y = onehot_labels[:,idx_e[:,b],1] |>gpu
+        grads = Flux.gradient(Flux.params(model)) do
+          y_pred = model(x)
+          l = loss(y_pred,y)
+        end
+        # epoch_loss +=Loss
+        Flux.update!(opt,Flux.params(model),grads)
+        
+        
+        print("batch: $b, epoch: $e \n ")
+        # Calculate accuracy for this batch
+        y_pred = model(x)
+        epoch_loss += loss(y_pred,y)
+        predictions = argmax(y_pred,dims=1)
+        correct_predictions += sum(predictions .== argmax(y, dims=1))
+        push!(losslistpara,loss(y_pred,y))
+        push!(accuracylistpara,sum(predictions .== argmax(y, dims=1))/2)
+
+        plt.plot(1:b,losslistpara[1:b],label="train")
+        plt.xlabel("epoch")
+        plt.ylabel("value")
+        plt.legend()
+        plt.title("Loss per update")
+        plt.savefig("/home/ykartha6/juliacode/figures for dis/loss batch $b.png")
+        plt.close()
+
+
+        plt.plot(1:b,accuracylistpara[1:b],label="train")
+        plt.xlabel("epoch")
+        plt.ylabel("value")
+        plt.legend()
+        plt.title("acc per update")
+        plt.savefig("/home/ykartha6/juliacode/figures for dis/acc batch $b.png")
+        plt.close()
+
+    end
+
+    idx_e_test = reshape(randperm(n_test), batch_size, n_batches_test)  
+    for b = 1:n_batches_test # batch loop
+      x = test_x[:,:,:,idx_e_test[:,b]]
+      x .+= 0.001*randn(Float32, size(x))
+      x = x |> gpu
+      y = onehot_labels_test[:,idx_e_test[:,b],1] |>gpu
+      
+      print("batch: $b \n")
+      # Calculate accuracy for this batch
+      y_pred = model(x)
+      epoch_loss_test += loss(y_pred,y)
+      predictions = argmax(y_pred,dims=1)
+      correct_predictions_test += sum(predictions .== argmax(y, dims=1))
+  end
+    
+    # Calculate average loss for the epoch
+    avg_epoch_loss_test = epoch_loss_test / size(idx_e_test, 2)
+    accuracy_test = correct_predictions_test / 300
+
+    push!(losslist_test, avg_epoch_loss_test)
+    push!(accuracylist_test, accuracy_test)
+
+    avg_epoch_loss = epoch_loss_test / size(idx_e, 2)
+    accuracy = correct_predictions / 1504
+    push!(losslist, avg_epoch_loss)
+    push!(accuracylist, accuracy)
+    
+    println("Epoch $e, Average Loss: $avg_epoch_loss, Accuracy: $accuracy% , Average Loss test: $avg_epoch_loss_test, Accuracy test: $accuracy_test%\n")
+
+    plt.plot(1:e,losslist[1:e],label="train")
+    plt.plot(1:e,losslist_test[1:e],label="test")
+    plt.xlabel("epoch")
+    plt.ylabel("value")
+    plt.legend()
+    plt.title("Loss per epoch")
+    plt.savefig("/home/ykartha6/juliacode/figures for dis/loss $e.png")
+    plt.close()
+
+
+    plt.plot(1:e,accuracylist[1:e],label="train")
+    plt.plot(1:e,accuracylist_test[1:e],label="test")
+    plt.xlabel("epoch")
+    plt.ylabel("value")
+    plt.legend()
+    plt.title("acc per epoch")
+    plt.savefig("/home/ykartha6/juliacode/figures for dis/acc $e.png")
+    plt.close()
+end
+
+plt.plot(losslist,label="train")
+plt.plot(losslist_test,label="test")
+plt.xlabel("epoch")
+plt.ylabel("value")
+plt.legend()
+plt.title("Loss per epoch")
+plt.savefig("/home/ykartha6/juliacode/figures for dis/loss.png")
+plt.close()
+
+
+plt.plot(accuracylist,label="train")
+plt.plot(accuracylist_test,label="test")
+plt.xlabel("epoch")
+plt.ylabel("value")
+plt.legend()
+plt.title("acc per epoch")
+plt.savefig("/home/ykartha6/juliacode/figures for dis/acc.png")
+plt.close()
