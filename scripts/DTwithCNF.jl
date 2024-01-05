ccbfccf6a193b170f066a8c2e4c0116812d2150c
YadhuKartha686 <yadhukartha@gatech.edu>
2024-01-05 10:47:37 -0500

    w

--- scripts/DTwithCNF.jl
+++ scripts/DTwithCNF.jl
@@ -50,7 +50,7 @@ split_scales = true
 low = 0.5f0
 
 # Architecture parametrs
-chan_x = 1; chan_y = 1; L = 4; K = 10; n_hidden = 128 # Number of hidden channels in convolutional residual blocks
+chan_x = 1; chan_y = 1; L = 5; K = 10; n_hidden = 128 # Number of hidden channels in convolutional residual blocks
 
 # Create network
 G = NetworkConditionalGlow(chan_x, chan_y, n_hidden,  L, K; split_scales=true,activation=SigmoidLayer(low=low,high=1.0f0)) |> device;
