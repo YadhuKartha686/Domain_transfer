db54106d251266147da125a51787c2af8441efb1
YadhuKartha686 <yadhukartha@gatech.edu>
2024-01-24 15:17:37 -0500

    s

--- scripts/DTwithCNF_mnist.jl
+++ scripts/DTwithCNF_mnist.jl
@@ -66,7 +66,7 @@ batch_size = 1
 low = 0.5f0
 
 # Architecture parametrs
-chan_x = 1; chan_y = 1; L = 3; K = 10; n_hidden = 64 # Number of hidden channels in convolutional residual blocks
+chan_x = 1; chan_y = 1; L = 4; K = 11; n_hidden = 64 # Number of hidden channels in convolutional residual blocks
 
 # Create network
 G = NetworkConditionalGlow(chan_x, chan_y, n_hidden,  L, K; split_scales=true,activation=SigmoidLayer(low=low,high=1.0f0)) |> device;
@@ -86,10 +86,10 @@ model = Chain(
 function Dissloss(real_output, fake_output)
   real_loss = mean(Flux.binarycrossentropy.(real_output, 1f0))
   fake_loss = mean(Flux.binarycrossentropy.(fake_output, 0f0))
-  return (real_loss + fake_loss)
+  return 0.5f0*(real_loss + fake_loss)
 end
 
-function Genloss(fake_output,x) 
+function Genloss(fake_output) 
   return mean(Flux.binarycrossentropy.(fake_output, 1f0))
 end
 
@@ -104,15 +104,15 @@ discriminatorB = gpu(model)
 # discriminatorB = model
 
 opt_adam = "adam"
-clipnorm_val = 1f0
+clipnorm_val = 100f0
 lrd = 1f-5
-optimizer_g = Flux.Optimiser(ClipNorm(clipnorm_val), ADAM(lrg))
-# optimizer_g = Flux.ADAM(lrg)
+# optimizer_g = Flux.Optimiser(ClipNorm(clipnorm_val), ADAM(lrg))
+optimizer_g = Flux.ADAM(lrg)
 optimizer_da = Flux.ADAM(lrd)
 optimizer_db = Flux.ADAM(lrd)
 genloss=[]
 dissloss = []
-imgs = 32
+imgs = 16
 n_train = 160
 n_test = 5200
 n_batches = cld(n_train,imgs)
@@ -120,7 +120,7 @@ YA = ones(Float32,16,16,1,imgs) + randn(Float32,16,16,1,imgs) ./1000
 YB = ones(Float32,16,16,1,imgs) .*7 + randn(Float32,16,16,1,imgs) ./1000
 
 lossnrm      = []; logdet_train = []; 
-factor = 1f-15
+factor = 1f-17
 
 n_epochs     = 1000
 for e=1:n_epochs# epoch loop
@@ -131,20 +131,13 @@ for e=1:n_epochs# epoch loop
   for b = 1:n_batches # batch loop
         @time begin
           ############# Loading domain A data ############## 
-          idx = reshape(randperm(imgs*2), imgs*2, 1)
-          inverse_idx = zeros(Int,length(idx))
-          for i in 1:length(idx)
-              inverse_idx[idx[i]] = i
-          end
           XA = train_xA[:, :, :, idx_eA[:,b]];
           XB = train_xB[:, :, :, idx_eA[:,b]];  
           X = cat(XA, XB,dims=4)
           Y = cat(YA, YB,dims=4)
-          X=X[:,:,:,idx[:]]
-          Y=Y[:,:,:,idx[:]]
+
           Zx, Zy, lgdet = generator.forward(X|> device, Y|> device)  #### concat so that network normalizes ####
-          Zx = Zx[:,:,:,inverse_idx[:]]
-          Zy = Zy[:,:,:,inverse_idx[:]]
+
 
           ######## interchanging conditions to get domain transferred images during inverse call #########
 
@@ -182,23 +175,16 @@ for e=1:n_epochs# epoch loop
             Flux.Optimise.update!(optimizer_db, Flux.params(discriminatorB),dB_grads)  #### domain B discrim ####
           
           ## minlog (1-D(fakeimg)) <--> max log(D(fake)) + norm(Z)
-          # mean((fake_output .- 1f0).^2)
 
-          gsA = gradient(x -> mean((discriminatorA(x|> device) .-1f0).^2), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
-          gsB = gradient(x -> mean((discriminatorB(x|> device) .-1f0).^2), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
+          # gsA = gradient(x -> mean((discriminatorA(x|> device) .-1f0).^2), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
+          # gsB = gradient(x -> mean((discriminatorB(x|> device) .-1f0).^2), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
           
-          # gsA = gradient(x -> mean(Flux.binarycrossentropy.(discriminatorA(x|> device), 1f0)), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
-          # gsB = gradient(x -> mean(Flux.binarycrossentropy.(discriminatorB(x|> device), 1f0)), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
-          
-          # gsA = gradient(x -> Genloss(discriminatorA(x|> device),x), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
-          # gsB = gradient(x -> Genloss(discriminatorB(x|> device),x), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
+          gsA = gradient(x -> Genloss(discriminatorA(x|> device)), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
+          gsB = gradient(x -> Genloss(discriminatorB(x|> device)), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
           
           gs = cat(gsB,gsA,dims=4)
-          Zx = cat(ZxB,ZxA,dims=4)
+          Zx = cat(ZxA,ZxB,dims=4)
           generator.backward_inv(((gs ./ factor)|>device) + Zx/(imgs*2), fake_images, invcall;) #### updating grads wrt image ####
-
-          # generator.backward_inv(((gsA ./ factor)|>device) + ZxA/4, fake_imagesAfromB, invcall[:,:,:,5:8];) #### updating grads wrt A ####
-          # generator.backward_inv(((gsB ./ factor)|>device) + ZxB/4, fake_imagesBfromA, invcall[:,:,:,1:4];) #### updating grads wrt B ####
 
           for p in get_params(generator)
               Flux.update!(optimizer_g,p.data,p.grad)
@@ -213,8 +199,8 @@ for e=1:n_epochs# epoch loop
 
           lossAd = Dissloss(real_outputA, fake_outputA)  #### log(D(real)) + log(1 - D(fake)) ####
           lossBd = Dissloss(real_outputB, fake_outputB)  #### log(D(real)) + log(1 - D(fake)) ####
-          lossA = Genloss(fake_outputA,fake_imagesAfromB)  #### log(1 - D(fake)) + mse ####
-          lossB = Genloss(fake_outputB,fake_imagesAfromB)  #### log(1 - D(fake)) + mse ####
+          lossA = Genloss(fake_outputA)  #### log(1 - D(fake)) + mse ####
+          lossB = Genloss(fake_outputB)  #### log(1 - D(fake)) + mse ####
           f_all = norm(Zx)^2
 
           loss = lossA + lossB #+ ml
@@ -222,16 +208,16 @@ for e=1:n_epochs# epoch loop
           append!(lossnrm, f_all / (imgs*2*N))  # normalize by image size and batch size
           append!(logdet_train, (-lgdet) / N) # logdet is internally normalized by batch size
           append!(genloss, loss)  # normalize by image size and batch size
-          append!(dissloss, (lossAd+lossBd)/2 ) # logdet is internally normalized by batch size
+          append!(dissloss, (lossAd+lossBd) ) # logdet is internally normalized by batch size
 
 
-          epoch_loss_diss += (lossAd+lossBd)/2
+          epoch_loss_diss += (lossAd+lossBd)
           epoch_loss_gen += loss
 
           println("Iter: epoch=", e, "/", n_epochs,":batch = ",b,
           "; Genloss=", loss, 
             "; genloss = ",  loss+(f_all / (imgs*2*N)), 
-              "; dissloss = ", (lossAd+lossBd)/2 , 
+              "; dissloss = ", (lossAd+lossBd) , 
               "; f l2 = ",  lossnrm[end], 
               "; lgdet = ", logdet_train[end], "\n")
 
@@ -246,28 +232,6 @@ for e=1:n_epochs# epoch loop
         end
 
         if mod(e,10) == 0 && mod(b,n_batches)==0
-          imshow(XA[:,:,:,1],vmin = 0,vmax = 1)
-          plt.title("data $e")
-          plt.savefig("../plots/Shot_rec_df/number one train$e.png")
-          plt.colorbar()
-          plt.close()
-
-          imshow(XB[:,:,:,1],vmin = 0,vmax = 1)
-          plt.title("data $e")
-          plt.savefig("../plots/Shot_rec_df/number seven train$e.png")
-          plt.colorbar()
-          plt.close()
-  
-          imshow(fake_imagesAfromB[:,:,1,1]|>cpu,vmin = 0,vmax = 1)
-          plt.title("digit pred 1 from 7 $e")
-          plt.savefig("../plots/Shot_rec_df/number one$e.png")
-          plt.colorbar()
-          plt.close()
-
-          imshow(fake_imagesBfromA[:,:,1,1]|>cpu,vmin = 0,vmax = 1)
-          plt.title("digit pred 7 from 1 $e")
-          plt.savefig("../plots/Shot_rec_df/number seven$e.png")
-          plt.close()
 
           plt.plot(lossnrm)
           plt.title("loss $e")
@@ -277,13 +241,10 @@ for e=1:n_epochs# epoch loop
           plt.title("logdet $e")
           plt.savefig("../plots/Shot_rec_df/logdet$e.png")
           plt.close()
-          plt.plot(1:e,genloss[1:e])
-          plt.title("genloss $e")
-          plt.savefig("../plots/Shot_rec_df/genloss$e.png")
-          plt.close()
-          plt.plot(1:e,dissloss[1:e])
-          plt.title("dissloss $e")
-          plt.savefig("../plots/Shot_rec_df/dissloss$e.png")
+          plt.plot(1:e,dissloss[1:e],label = "dissloss")
+          plt.plot(1:e,genloss[1:e] ,label = "genloss")
+          plt.title("gan loss $e")
+          plt.savefig("../plots/Shot_rec_df/ganloss$e.png")
           plt.close()
         end
 
@@ -384,111 +345,3 @@ end
 
 
 print("done training!!!")
-
-
-##### testing ##########
-XA = zeros(Float32 , 16,16,1,imgs)
-XB = zeros(Float32 , 16,16,1,imgs)
-XA[:,:,:,1:imgs] = train_xA[:,:,:,n_test:n_test-1+imgs]
-XB[:,:,:,1:imgs] = train_xB[:,:,:,n_test:n_test-1+imgs]
-Z_fix =  randn(Float32,16,16,1,imgs*2)
-YA = ones(Float32,size(XA)) + randn(Float32,size(XA)) ./1000
-YB = ones(Float32,size(XB)) .*7 + randn(Float32,size(XB)) ./1000
-
-X = cat(XA, XB,dims=4)
-Y = cat(YA, YB,dims=4)
-Zx, Zy, lgdet = generator.forward(X|> device, Y|> device)  #### concat so that network normalizes ####
-
-
-          ######## interchanging conditions to get domain transferred images during inverse call #########
-
-ZyA = Zy[:,:,:,1:imgs]
-ZyB = Zy[:,:,:,imgs+1:end]
-
-Zy = cat(ZyB,ZyA,dims=4)
-
-fake_images,invcall = generator.inverse(Zx|>device,Zy)  ###### generating images #######
-
-          ####### getting fake images from respective domain ########
-
-fake_imagesAfromB = fake_images[:,:,:,imgs+1:end]
-fake_imagesBfromA = fake_images[:,:,:,1:imgs]
-
-fig = plt.figure(figsize=(15, 15))
-ax1 = fig.add_subplot(3,2,1)
-ax1.imshow(XB[:,:,:,1],vmin = 0,vmax = 1)
-ax1.title.set_text("data test ")
-
-
-ax2 = fig.add_subplot(3,2,2)
-ax2.imshow(fake_imagesAfromB[:,:,1,1]|>cpu,vmin = 0,vmax = 1)
-ax2.title.set_text("digit pred 1 from 7 ")
-
-
-ax3 = fig.add_subplot(3,2,3)
-ax3.imshow(XB[:,:,:,2],vmin = 0,vmax = 1)
-ax3.title.set_text("data test ")
-
-
-ax4 = fig.add_subplot(3,2,4)
-ax4.imshow(fake_imagesAfromB[:,:,1,2]|>cpu,vmin = 0,vmax = 1)
-ax4.title.set_text("digit pred 1 from 7 ")
-
-
-
-ax5 = fig.add_subplot(3,2,5)
-ax5.imshow(XB[:,:,:,3],vmin = 0,vmax = 1)
-ax5.title.set_text("data test ")
-
-
-ax6 = fig.add_subplot(3,2,6)
-ax6.imshow(fake_imagesAfromB[:,:,1,3]|>cpu,vmin = 0,vmax = 1)
-ax6.title.set_text("digit pred 1 from 7 ")
-
-
-fig.savefig("../plots/Shot_rec_df/number one test.png")
-plt.close(fig)
-
-
-fig = plt.figure(figsize=(15, 15))
-ax1 = fig.add_subplot(3,2,1)
-ax1.imshow(XA[:,:,:,1],vmin = 0,vmax = 1)
-ax1.title.set_text("data test ")
-
-
-ax2 = fig.add_subplot(3,2,2)
-ax2.imshow(fake_imagesBfromA[:,:,1,1]|>cpu,vmin = 0,vmax = 1)
-ax2.title.set_text("digit pred 7 from 1 ")
-
-ax3 = fig.add_subplot(3,2,3)
-ax3.imshow(XA[:,:,:,2],vmin = 0,vmax = 1)
-ax3.title.set_text("data test ")
-
-
-ax4 = fig.add_subplot(3,2,4)
-ax4.imshow(fake_imagesBfromA[:,:,1,2]|>cpu,vmin = 0,vmax = 1)
-ax4.title.set_text("digit pred 7 from 1 ")
-
-
-ax5 = fig.add_subplot(3,2,5)
-ax5.imshow(XA[:,:,:,4],vmin = 0,vmax = 1)
-ax5.title.set_text("data test ")
-
-
-ax6 = fig.add_subplot(3,2,6)
-ax6.imshow(fake_imagesBfromA[:,:,1,4]|>cpu,vmin = 0,vmax = 1)
-ax6.title.set_text("digit pred 7 from 1 ")
-
-
-fig.savefig("../plots/Shot_rec_df/number seven test.png")
-plt.close(fig)
-
-
-# include("/home/ykartha6/juliacode/Domain_transfer/scripts/DTwithCNF_mnist.jl")
-
-
-
-# plt.title("gradient")
-# plt.savefig("../plots/Shot_rec_df/gradient when putting flux loss.png")
-# plt.colorbar()
-# plt.close()
