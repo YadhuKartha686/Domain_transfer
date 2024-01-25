df397dc937820579b0c123f41b9f07771a181f0b
YadhuKartha686 <yadhukartha@gatech.edu>
2024-01-24 20:32:04 -0500

    w

--- scripts/DTwithCNF_mnist.jl
+++ scripts/DTwithCNF_mnist.jl
@@ -60,33 +60,33 @@ N = nx*ny;
 
 
 device = gpu #GPU does not accelerate at this small size. quicker on cpu
-lrg     = 1f-5
+lr     = 1f-6
 epochs = 30
 batch_size = 1
 low = 0.5f0
 
 # Architecture parametrs
-chan_x = 1; chan_y = 1; L = 2; K = 5; n_hidden = 64 # Number of hidden channels in convolutional residual blocks
+chan_x = 1; chan_y = 1; L = 4; K = 10; n_hidden = 128 # Number of hidden channels in convolutional residual blocks
 
 # Create network
 G = NetworkConditionalGlow(chan_x, chan_y, n_hidden,  L, K; split_scales=true,activation=SigmoidLayer(low=low,high=1.0f0)) |> device;
 
 model = Chain(
-    Conv((3, 3), 1=>64, relu;pad = 1),
+    Conv((3, 3), 1=>64, relu;pad=1),
     x -> maxpool(x, (2,2)),
     Conv((3, 3), 64=>32, relu;pad=1),
     x -> maxpool(x, (2,2)),
     x -> reshape(x, :, size(x, 4)),
-    Dense(512, 1),
+    Dense(128, 1),
     sigmoid
 )
 
 
 # Define the loss functions
 function Dissloss(real_output, fake_output)
-  real_loss = mean(Flux.binarycrossentropy.(real_output, 1f0))
+  real_loss = 10*mean(Flux.binarycrossentropy.(real_output, 1f0))
   fake_loss = mean(Flux.binarycrossentropy.(fake_output, 0f0))
-  return 0.5f0*(real_loss + fake_loss)
+  return (real_loss + fake_loss)
 end
 
 function Genloss(fake_output) 
@@ -105,24 +105,22 @@ discriminatorB = gpu(model)
 
 opt_adam = "adam"
 clipnorm_val = 5f0
-lrd = 1f-5
-optimizer_g = Flux.Optimiser(ClipNorm(clipnorm_val), ADAM(lrg))
-# optimizer_g = Flux.ADAM(lrg)
-optimizer_da = Flux.ADAM(lrd)
-optimizer_db = Flux.ADAM(lrd)
+optimizer_g = Flux.Optimiser(ClipNorm(clipnorm_val), ADAM(lr))
+optimizer_da = Flux.ADAM(lr)
+optimizer_db = Flux.ADAM(lr)
 genloss=[]
 dissloss = []
-imgs = 16
-n_train = 160
-n_test = 5200
+imgs = 32
+n_train = 320
+n_test = 4500
 n_batches = cld(n_train,imgs)
 YA = ones(Float32,16,16,1,imgs) + randn(Float32,16,16,1,imgs) ./1000
 YB = ones(Float32,16,16,1,imgs) .*7 + randn(Float32,16,16,1,imgs) ./1000
 
 lossnrm      = []; logdet_train = []; 
-factor = 1f-19
+factor = 1f-5
 
-n_epochs     = 1000
+n_epochs     = 500
 for e=1:n_epochs# epoch loop
   epoch_loss_diss=0.0
   epoch_loss_gen=0.0
@@ -131,13 +129,20 @@ for e=1:n_epochs# epoch loop
   for b = 1:n_batches # batch loop
         @time begin
           ############# Loading domain A data ############## 
+          idx = reshape(randperm(imgs*2), imgs*2, 1)
+          inverse_idx = zeros(Int,length(idx))
+          for i in 1:length(idx)
+              inverse_idx[idx[i]] = i
+          end
           XA = train_xA[:, :, :, idx_eA[:,b]];
           XB = train_xB[:, :, :, idx_eA[:,b]];  
           X = cat(XA, XB,dims=4)
           Y = cat(YA, YB,dims=4)
-
+          X=X[:,:,:,idx[:]]
+          Y=Y[:,:,:,idx[:]]
           Zx, Zy, lgdet = generator.forward(X|> device, Y|> device)  #### concat so that network normalizes ####
-
+          Zx = Zx[:,:,:,inverse_idx[:]]
+          Zy = Zy[:,:,:,inverse_idx[:]]
 
           ######## interchanging conditions to get domain transferred images during inverse call #########
 
@@ -147,18 +152,21 @@ for e=1:n_epochs# epoch loop
           ZxA = Zx[:,:,:,1:imgs]
           ZxB = Zx[:,:,:,imgs+1:end]
 
-          Zy = cat(ZyB,ZyA,dims=4)       
+          Zy = cat(ZyB,ZyA,dims=4)
+
+          Zx=Zx[:,:,:,idx[:]]
+          Zy=Zy[:,:,:,idx[:]]
 
           fake_images,invcall = generator.inverse(Zx|>device,Zy)  ###### generating images #######
-
+          fake_images = fake_images[:,:,:,inverse_idx[:]]
+          invcall = invcall[:,:,:,inverse_idx[:]]
           ####### getting fake images from respective domain ########
 
           fake_imagesAfromB = fake_images[:,:,:,imgs+1:end]
           fake_imagesBfromA = fake_images[:,:,:,1:imgs]
 
           ####### discrim training ########
-          
-          
+          for ii=1:2
             dA_grads = Flux.gradient(Flux.params(discriminatorA)) do
                 real_outputA = discriminatorA(XA|> device)
                 fake_outputA = discriminatorA(fake_imagesAfromB|> device)
@@ -173,18 +181,19 @@ for e=1:n_epochs# epoch loop
                 lossB = Dissloss(real_outputB, fake_outputB)
             end
             Flux.Optimise.update!(optimizer_db, Flux.params(discriminatorB),dB_grads)  #### domain B discrim ####
-          
+          end
           ## minlog (1-D(fakeimg)) <--> max log(D(fake)) + norm(Z)
-
-          # gsA = gradient(x -> mean((discriminatorA(x|> device) .-1f0).^2), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
-          # gsB = gradient(x -> mean((discriminatorB(x|> device) .-1f0).^2), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
-          
+                    
           gsA = gradient(x -> Genloss(discriminatorA(x|> device)), fake_imagesAfromB)[1]  #### getting gradients wrt A fake ####
           gsB = gradient(x -> Genloss(discriminatorB(x|> device)), fake_imagesBfromA)[1]  #### getting gradients wrt B fake ####
           
+
           gs = cat(gsB,gsA,dims=4)
-          Zx = cat(ZxA,ZxB,dims=4)
-          generator.backward_inv(((gs ./ factor)|>device) + Zx/(imgs*2), fake_images, invcall;) #### updating grads wrt image ####
+          Zx = cat(ZxB,ZxA,dims=4)
+          generator.backward_inv(((gs ./ factor)|>device) + Zx/(imgs*2*1f5), fake_images, invcall;) #### updating grads wrt image ####
+
+          # generator.backward_inv(((gsA ./ factor)|>device) + ZxA/4, fake_imagesAfromB, invcall[:,:,:,5:8];) #### updating grads wrt A ####
+          # generator.backward_inv(((gsB ./ factor)|>device) + ZxB/4, fake_imagesBfromA, invcall[:,:,:,1:4];) #### updating grads wrt B ####
 
           for p in get_params(generator)
               Flux.update!(optimizer_g,p.data,p.grad)
@@ -232,7 +241,6 @@ for e=1:n_epochs# epoch loop
         end
 
         if mod(e,10) == 0 && mod(b,n_batches)==0
-
           plt.plot(lossnrm)
           plt.title("loss $e")
           plt.savefig("../plots/Shot_rec_df/lossnorm$e.png")
@@ -241,9 +249,11 @@ for e=1:n_epochs# epoch loop
           plt.title("logdet $e")
           plt.savefig("../plots/Shot_rec_df/logdet$e.png")
           plt.close()
-          plt.plot(1:e,dissloss[1:e],label = "dissloss")
-          plt.plot(1:e,genloss[1:e] ,label = "genloss")
-          plt.title("gan loss $e")
+      
+          plt.plot(1:e,dissloss[1:e],label = "Diss")
+          plt.plot(1:e,genloss[1:e], label = "Gen")
+          plt.title("ganloss $e")
+          plt.legend()
           plt.savefig("../plots/Shot_rec_df/ganloss$e.png")
           plt.close()
         end
