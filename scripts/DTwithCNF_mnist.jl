72d8098c23873660b2722709fbb79ad55942e4da
YadhuKartha686 <yadhukartha@gatech.edu>
2024-02-19 13:24:25 -0500

    w

--- scripts/DTwithCNF_mnist.jl
+++ scripts/DTwithCNF_mnist.jl
@@ -66,7 +66,7 @@ batch_size = 1
 low = 0.5f0
 
 # Architecture parametrs
-chan_x = 1; chan_y = 1; L = 4; K = 10; n_hidden = 128 # Number of hidden channels in convolutional residual blocks
+chan_x = 1; chan_y = 1; L = 2; K = 10; n_hidden = 128 # Number of hidden channels in convolutional residual blocks
 
 # Create network
 G = NetworkConditionalGlow(chan_x, chan_y, n_hidden,  L, K; split_scales=true,activation=SigmoidLayer(low=low,high=1.0f0)) |> device;
@@ -121,7 +121,7 @@ optimizer_db = Flux.ADAM(lrd)
 genloss=[]
 dissloss = []
 imgs = 32
-n_train = 1024
+n_train = 2048
 n_test = 4500
 n_batches = cld(n_train,imgs)
 YA = ones(Float32,16,16,1,imgs) + randn(Float32,16,16,1,imgs) ./1000
