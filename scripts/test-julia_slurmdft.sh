0abbefa7c5deeb5054ca07c785c4c558aff0fa3d
YadhuKartha686 <yadhukartha@gatech.edu>
2024-04-08 11:05:22 -0400

    w

--- scripts/test-julia_slurmdft.sh
+++ scripts/test-julia_slurmdft.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=CNFDT_test
 #SBATCH --output=outputdt.txt
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
 #SBATCH --time=150:00:00
 #SBATCH --gres=gpu:1
